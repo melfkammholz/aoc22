ebd32ed3c6e15a4cd98d59252b13dd91184e543a
Melf Kammholz <melf.kammholz@gmail.com>
2022-12-06 08:54:15 +0100

    refactor day 06

--- day06/B.hs
+++ day06/B.hs
@@ -1,16 +1,18 @@
 import Data.List (nub)
 
-solve :: Int -> Int -> String -> String -> Maybe Int
-solve _ _ _ []    = Nothing
-solve i k r (c:b) =
-  let r' = r ++ [c]
-  in if length (nub r') == k
-     then Just i
-     else solve (i + 1) k (tail r') b
+solve :: Int -> String -> Maybe Int
+solve k l = solve' k (take (k - 1) l) (drop (k - 1) l)
+  where
+    solve' _ _ []    = Nothing
+    solve' i r (c:b) =
+      let r' = r ++ [c]
+      in if length (nub r') == k
+         then Just i
+         else solve' (i + 1) (tail r') b
 
 main :: IO ()
 main = do
   l <- getLine
   let k = 14
-  print $ solve k k (take (k - 1) l) (drop (k - 1) l)
+  print $ solve k l
 
